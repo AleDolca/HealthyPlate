3d64a4557ba6f9561ef25e759d97db791e15f422
AleDolca <103220820+AleDolca@users.noreply.github.com>
2022-05-12 19:22:03 +0300

    HP-32 design formular de conectare

---
+++ src/main/java/com/example/projecthp/ConectController.java
@@ -0,0 +1,23 @@
+package com.example.projecthp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+public class ConectController {
+    @FXML
+    private Button buton_conectare;
+
+    @FXML
+    private Button buton_inregistrare;
+
+    @FXML
+    private TextField txt_nume;
+
+    @FXML
+    private TextField txt_parola;
+
+    @FXML
+    void onClickButton(ActionEvent event) {
+
+    }
+}
