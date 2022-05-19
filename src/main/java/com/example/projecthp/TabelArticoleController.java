a64ec885c0981174e3e5fd788ad3a6a967509e8f
Draghin Alexia <alexia.draghin@student.upt.ro>
2022-05-19 19:23:29 +0300

    HP-45 am configurat tabelul cu articole

--- src/main/java/com/example/projecthp/TabelArticoleController.java
+++ src/main/java/com/example/projecthp/TabelArticoleController.java
@@ -11,15 +11,47 @@ public class TabelArticoleController {
     private Button buton_inapoi;
 
     @FXML
-    private TableColumn<?, ?> c_articol;
+    private TableColumn<ClasaArticole, String> c_articol;
 
     @FXML
-    private TableColumn<?, ?> c_autor;
+    private TableColumn<ClasaArticole, String> c_autor;
 
     @FXML
-    private TableColumn<?, ?> c_link;
+    private TableColumn<ClasaArticole, String> c_link;
 
     @FXML
-    private TableView<?> tabel;
+    private TableView<ClasaArticole> tabel;
+
+    ObservableList<ClasaArticole> listaArticole = FXCollections.observableArrayList();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ConexiuneArticole connectNow = new ConexiuneArticole();
+        Connection connectDB = connectNow.getDBConnection();
+
+        String productViewQuery = "SELECT * FROM articole";
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOut = statement.executeQuery(productViewQuery);
+
+            while (queryOut.next()){
+                String qautor=queryOut.getString("nume_autor");
+                String qarticol=queryOut.getString("nume_articol");
+                String qlink=queryOut.getString("link");
+                listaArticole.add(new ClasaArticole(qautor, qarticol, qlink));
+
+            }
+
+            c_autor.setCellValueFactory(new PropertyValueFactory<>("nume_autor"));
+            c_articol.setCellValueFactory(new PropertyValueFactory<>("nume_articol"));
+            c_link.setCellValueFactory(new PropertyValueFactory<>("link"));
+
+            tabel.setItems(listaArticole);
+
+        }catch (SQLException e){
+            Logger.getLogger(TabelArticoleController.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+    }
 
 }
